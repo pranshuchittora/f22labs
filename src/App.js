ad4dd8136db8d41728280412e6a3add140f63b04
Pranshu Chittora <pranshuchittora17@gmail.com>
2018-10-19 18:03:21 +0530

    Before yarn eject

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import Login from './components/Login/LoginForm'
+import Grid from '@material-ui/core/Grid'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <h1>Hola Amigo</h1>
+      <div>
+        <Grid container justify="center" >
+          <Grid item xs={12} sm={8} md={6} lg={4}>
+            <Login />
+          </Grid>
+        </Grid>
       </div>
     );
   }
